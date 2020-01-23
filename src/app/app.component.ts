d8623d1eab038d0675700bb4de176dd9ee9d1b53
felipe ferrer <luisfferrer12@gmail.com>
2020-01-22 23:06:35 -0500

    pipes primera parte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = "Felipe";
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10,11];
+
+  PI = Math.PI;
+
+  num = 0.234;
+
+  val = 1234.5;
+
+  heroe = {
+    nombre: 'Peter',
+    clave: 'Spiderman',
+    edad: '29',
+    direccion: {
+      calle: 'primera',
+      casa: 116
+    }
+  };
 }
